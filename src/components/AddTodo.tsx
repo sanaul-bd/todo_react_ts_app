10fc7c43032bb2be259308e5439290fe5d2c27e4
sanaul-bd <sanaullah.bd21@gmail.com>
2025-07-19 00:37:06 +0600

    add useContext functionality

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,24 +1,28 @@
-import { useState } from "react";
+import { useState, type FormEvent } from "react";
 
 const AddTodo = () => {
-  const [todo, setTodo] = useState("");
-  console.log(todo);
-  const handleFormSubmit = () => {};
-  return (
-    <div>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          name=""
-          id=""
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
-        />
-        <button type="submit">Add</button>
-      </form>
-      {todo}
-    </div>
-  );
+    const [todo, setTodo] = useState("");
+    console.log(todo);
+
+    // func
+    const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        // const handleAddTodo(todo);
+        setTodo("")
+    };
+    return (
+        <form onSubmit={(e) => handleFormSubmit}>
+            <input
+                type="text"
+                name=""
+                id=""
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+            />
+            <button type="submit">Add</button>
+        </form>
+        // {todo}
+    );
 };
 
 export default AddTodo;
---
+++ src/store/todos.tsx
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+
+
+export const todosContext = createContext(null);
+export const TodosProvider = ({ children }) => {
+    return <todosContext.Provider>
+        {children}
+    </todosContext.Provider>
+}
